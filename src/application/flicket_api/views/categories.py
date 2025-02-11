7493f21eb08cc7eb780b280b5df9f4eb2afe74c6
giti-jahanpasand <gitijahanpasand@gmail.com>
2025-02-11 11:56:35 +0330

    change apis

--- src/application/flicket_api/views/categories.py
+++ src/application/flicket_api/views/categories.py
@@ -134,50 +134,58 @@ from application.flicket_api.views.auth import token_auth
 from application.flicket_api.views.errors import bad_request
 
 
-@bp_api.route(api_url + 'category/<int:id>', methods=['GET'])
+@bp_api.route(api_url + "category/<int:id>", methods=["GET"])
 @token_auth.login_required
 def get_category(id):
     return jsonify(FlicketCategory.query.get_or_404(id).to_dict())
 
 
-@bp_api.route(api_url + 'categories/', methods=['GET'])
+@bp_api.route(api_url + "categories/", methods=["GET"])
 @token_auth.login_required
 def get_categories():
-    department_id = request.args.get('department_id')
-    department = request.args.get('department')
+    department_id = request.args.get("department_id")
+    department = request.args.get("department")
     categories = FlicketCategory.query.order_by(FlicketCategory.category.asc())
     if department_id:
         categories = categories.filter_by(department_id=department_id)
     if department:
-        categories = categories.filter(FlicketCategory.department.has(FlicketDepartment.department == department))
-    page = request.args.get('page', 1, type=int)
-    per_page = min(request.args.get('per_page', app.config['posts_per_page'], type=int), 100)
-    data = FlicketCategory.to_collection_dict(categories, page, per_page, 'bp_api.get_categories')
+        categories = categories.filter(
+            FlicketCategory.department.has(FlicketDepartment.department == department)
+        )
+    page = request.args.get("page", 1, type=int)
+    per_page = min(
+        request.args.get("per_page", app.config["posts_per_page"], type=int), 100
+    )
+    data = FlicketCategory.to_collection_dict(
+        categories, page, per_page, "bp_api.get_categories"
+    )
     return jsonify(data)
 
 
-@bp_api.route(api_url + 'categories', methods=['POST'])
+@bp_api.route(api_url + "categories", methods=["POST"])
 @token_auth.login_required
 def create_category():
     data = request.get_json() or {}
 
-    if 'category' not in data or 'department_id' not in data:
-        return bad_request('Must include category name and department_id')
+    if "category" not in data or "department_id" not in data:
+        return bad_request("Must include category name and department_id")
 
-    if not isinstance(data['department_id'], int):
-        return bad_request('department_id must be an integer.')
+    if not isinstance(data["department_id"], int):
+        return bad_request("department_id must be an integer.")
 
-    if FlicketCategory.query. \
-            filter_by(category=data['category']). \
-            filter_by(department_id=data['department_id']).first():
-        return bad_request('Category within department already exists.')
+    if (
+        FlicketCategory.query.filter_by(category=data["category"])
+        .filter_by(department_id=data["department_id"])
+        .first()
+    ):
+        return bad_request("Category within department already exists.")
 
-    department = FlicketDepartment.query.filter_by(id=data['department_id']).first()
-    category = FlicketCategory(data['category'], department)
+    department = FlicketDepartment.query.filter_by(id=data["department_id"]).first()
+    category = FlicketCategory(data["category"], department)
     db.session.add(category)
     db.session.commit()
 
     response = jsonify(category.to_dict())
     response.status_code = 201
-    response.headers['Location'] = url_for('bp_api.get_category', id=category.id)
+    response.headers["Location"] = url_for("bp_api.get_category", id=category.id)
     return response
--- src/application/flicket_api/views/subscriptions.py
+++ src/application/flicket_api/views/subscriptions.py
@@ -111,21 +111,24 @@ from application.flicket.models.flicket_models import FlicketSubscription
 from application.flicket_api.views.auth import token_auth
 
 
-@bp_api.route(api_url + 'subscription/<int:id>', methods=['GET'])
+@bp_api.route(api_url + "subscription/<int:id>", methods=["GET"])
 @token_auth.login_required
 def get_subscription(id):
     return jsonify(FlicketSubscription.query.get_or_404(id).to_dict())
 
 
-@bp_api.route(api_url + 'subscriptions/', methods=['GET'])
-@bp_api.route(api_url + 'subscriptions/<int:ticket_id>/', methods=['GET'])
-@bp_api.route(api_url + 'subscriptions/<int:ticket_id>/<int:page>/', methods=['GET'])
+@bp_api.route(api_url + "subscriptions/", methods=["GET"])
+@bp_api.route(api_url + "subscriptions/<int:ticket_id>/", methods=["GET"])
+@bp_api.route(api_url + "subscriptions/<int:ticket_id>/<int:page>/", methods=["GET"])
 @token_auth.login_required
 def get_subscriptions(page=1, ticket_id=None):
     subscriptions = FlicketSubscription.query
     if ticket_id:
         subscriptions = subscriptions.filter_by(ticket_id=ticket_id)
-    per_page = min(request.args.get('per_page', app.config['posts_per_page'], type=int), 100)
-    data = FlicketSubscription.to_collection_dict(subscriptions, page, per_page, 'bp_api.get_subscriptions',
-                                                  ticket_id=ticket_id)
+    per_page = min(
+        request.args.get("per_page", app.config["posts_per_page"], type=int), 100
+    )
+    data = FlicketSubscription.to_collection_dict(
+        subscriptions, page, per_page, "bp_api.get_subscriptions", ticket_id=ticket_id
+    )
     return jsonify(data)
